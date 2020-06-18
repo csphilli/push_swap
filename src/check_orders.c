feabaa2c95f943b2cb0f5bbf4ecf7a96b3541ae0
Christopher <csphilli@gmail.com>
2020-06-18 15:17:49 +0300

    protected check_orders functions

--- src/check_orders.c
+++ src/check_orders.c
@@ -6,7 +6,7 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 13:26:18 by cphillip          #+#    #+#             */
-/*   Updated: 2020/06/15 13:29:25 by cphillip         ###   ########.fr       */
+/*   Updated: 2020/06/18 15:14:38 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	check_order_a(t_stacks *stacks)
 	head = stacks->head_a;
 	second = stacks->head_a;
 	stacks->a_sorted = false;
-	if (head)
+	if (head && head != NULL)
 	{
 		second = head->next;
 		while (second != NULL)
@@ -50,7 +50,7 @@ void	check_order_b(t_stacks *stacks)
 	head = stacks->head_b;
 	second = stacks->head_b;
 	stacks->b_sorted = false;
-	if (head != NULL)
+	if (head && head != NULL)
 	{
 		second = head->next;
 		while (second != NULL)
@@ -67,7 +67,7 @@ void	check_order_b(t_stacks *stacks)
 
 void	check_full_sort(t_stacks *stacks)
 {
-	if (stacks->head_a->next != NULL)
+	if (stacks->head_a && stacks->head_a->next != NULL)
 	{
 		if (stacks->head_b != NULL)
 			stacks->full_sort = false;
